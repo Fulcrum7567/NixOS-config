e4d5451bbf0f981c6640aa1b00c3651d541fe2d0
Fulcrum <dragon.fighter@outlook.de>
2024-12-03 00:32:17 +0100

    added read hostname

--- system/scripts/interactive/registerHost.sh
+++ system/scripts/interactive/registerHost.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-host_name=""
+hostname=""
 debug=false
 no_new_config=false
 force=false
@@ -32,10 +32,30 @@ print_debug() {
     fi
 }
 
+get_confirmation() {
+	local prompt_message="$1"
+	while true; do
+		read -rp "$prompt_message (y/n): " choice
+		case "$choice" in
+			[yY] | [yY][eE][sS] )
+				return 0
+				;;
+			[nN] | [nN][oO] )
+				return 1
+				;;
+			* )
+				echo "Please answer yes or no."
+				;;
+		esac
+	done
+}
+
 if [ "$1" = "--help" -o "$1" = "-h" ]; then
     print_usage_force
     exit 0
 fi
+
+
 
 while [ $# -gt 0 ]; do
     case "$1" in
@@ -53,7 +73,7 @@ while [ $# -gt 0 ]; do
             ;;
 		--hostname|-n)
 			if [ -n "$2" ]; then
-				host_name="$2"
+				hostname="$2"
 				shift
 			else
 				echo "Error: --hostname or -n requires a name."
@@ -69,8 +89,23 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-echo $host_name
-echo $debug
-echo $no_new_config
-echo $force
-echo $no_usage
+repeat=true
+while [ $repeat ]; do
+	repeat=false
+	read -p "What is the name of the host? " $hostname
+	if [ -z "$hostname" ]; then
+		echo "Error: Hostname must not be empty!"
+		repeat=true
+	elif [ -d "$path_to_dotfiles/hosts/$hostname/" ]; then
+		echo "Warning: A host with the name $hostname is already registered."
+		get_confirmation "Do you want to overwrite it?"
+		if [ "$?" = 0 ]; then
+			force=true
+		else
+			hostname=""
+			force=false
+			repeat=true
+		fi
+	fi
+done
+	
