a72c63da8749ba79428191de7f4333e35cf4a2e2
Fulcrum7567 <dragon.fighter@outlook.de>
2025-03-01 00:19:38 +0100

    fixed addPackage

--- system/scripts/interactive/addPackage.sh
+++ system/scripts/interactive/addPackage.sh
@@ -7,21 +7,31 @@ path_to_dotfiles=$(realpath "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../..
 cmd_debug=""
 cmd_no_usage=""
 
+name=""
+group=""
+group_given=false
+binary=false
+binary_name=""
+no_git=false
+state=""
+edited_file=""
 
 
 print_usage_force() {
     echo "Usage:" 
     echo "  $0 [option?]"
     echo "Or:"
-    echo "  $0 --help, -h      Show this message and exit"
+    echo "  $0 --help, -h                    Show this message and exit"
     echo ""
     echo "Options:"
-	echo "  --name,    -n <package name>	  Pre set package name"
-    echo "  --group,   -g <group name>        Pre set group"            
-    echo "  --no-edit,   -e 				Do not ask for group editing"            
+	echo "  --name,    -n <package name>	              Pre set package name"
+    echo "  --group,   -g <group name>                  Pre set group"
+    echo "  --state,   -s <Stable | Unstable | Default> Pre set the package state"
+    echo "  --binary,  -b                               Set up package as binary" 
+    echo "  --no-git                                    Don't add edits to git"         
     echo ""
-    echo "  --no-usage, -u                  Don't show usage after an error"
-    echo "  --debug,    -d                  Enable debug mode"
+    echo "  --no-usage, -u                              Don't show usage after an error"
+    echo "  --debug,    -d                              Enable debug mode"
     echo ""
 }
 
@@ -41,12 +51,29 @@ print_error() {
     echo "[Error]: $1"
 }
 
-
-is_hostname_valid() {
-    if [ -z "$(echo "$hostname" | xargs)" -o "$hostname" = "GLOBAL" ]; then
-        return 1
+add_to_git() {
+    if [ "$no_git" = false ]; then
+        gum confirm "Would you like to add your changes to git?"
+        if [ "$?" = 0 ]; then
+            git add "$1"
+            git commit -m "[Auto generated] $2"
+        fi
     fi
-    return 0
+}
+
+choose_group() {
+    group=$(find "$path_to_dotfiles/user/packages/groups" -type f -printf "%P\n" | sed 's/\.nix$//' | awk 'END {print "NEW"} {print}' | gum choose --header="What desktop profile do you want to use?")
+    if [ "$group" = "NEW" ]; then
+        group=$(gum input --prompt="What should the name of the group be? " --placeholder="Enter name...")
+        while [ "$group" = "NEW" ]; do
+            print_error "The group name \"NEW\" is reserved."
+            group=$(gum input --prompt="What should the name of the group be? " --placeholder="Enter name...")
+        done
+    fi
+    if [ -z "$group" ]; then
+        exit 1
+    fi
+
 }
 
 
@@ -68,22 +95,45 @@ while [ $# -gt 0 ]; do
             no_usage=true
             cmd_no_usage="--no-usage"
             ;;
-        --copy-config|-c)
-            copy_config=true
+        --binary|-b)
+            binary=true
             ;;
-        --overwrite|-o)
-            overwrite=true
+        --no-git)
+            no_git=true
             ;;
-        --repair|-r)
-            repair=true
+        --state|-s)
+            if [ -n "$2" ]; then
+                case "$2" in
+                    Stable|Unstable|Default)
+                        state="$2"
+                        ;;
+                    *)
+                        print_error "Invalid state: \"$2\""
+                        exit 2
+                        ;;
+                esac
+            else
+                print_error "--state, -s requires a state"
+                exit 2
+            fi
             ;;
-		--hostname|-H)
+        --group|-g)
+            if [ -n "$2" ]; then
+                group="$2"
+                group_given=true
+                shift
+            else
+                print_error "--group, -g requires a group name"
+                print_usage
+                exit 1
+            fi
+            ;;
+		--name|-n)
 			if [ -n "$2" ]; then
-				hostname="$2"
-                hostname_given=true
+				name="$2"
 				shift
 			else
-				print_error "--hostname, -H requires a hostname"
+				print_error "--name, -n requires a package name"
 				print_usage
                 exit 1
 			fi
@@ -97,7 +147,126 @@ while [ $# -gt 0 ]; do
     shift
 done
 
+if [ "$binary" = true -a "$group_given" = true ]; then
+    print_error "Binary and group must not be used together"
+    exit 2
+fi
 
+
+if [ -z "$name" ]; then
+    name=$(gum input --prompt="What package would you like to install? " --placeholder="Enter name...")
+fi
+
+if [ -z "$name" ]; then
+    print_debug "No name set, exiting..."
+    exit 1
+fi
+
+if [ -z "$state" ]; then
+    state=$(gum choose Default Stable Unstable --header="What state should the package be in?")
+fi
+
+if [ -z "$state" ]; then
+    print_debug "No state selected, exiting"
+    exit 1
+fi
+
+
+if [ "$binary" = false -a -z "$group" ]; then
+    result=$(gum choose Binary Group Cancel --header="Would you install the package as binary or in a group?")
+    if [ "$result" = "Binary" ]; then
+        binary=true
+    elif [ "$result" = "Group" ]; then
+        choose_group
+    else
+        exit 1
+    fi
+fi
+
+insertion=$'\t\t'"$name"
+pattern=""
+message=""
+
+if [ "$binary" = true ]; then
+    binary_name=$(gum input --prompt="Under what name would you like to create a binary file? " --placeholder="Default: $name")
+    if [ -z "$binary_name" ]; then
+        binary_name="$name"
+    fi
+    if [ -f "$path_to_dotfiles/user/packages/binaries/$binary_name.nix" ]; then
+        gum confirm "A binary file with the name \"$binary_name\" already exists. Do you want to edit it?"
+        if [ "$?" -eq 0 ]; then
+            $EDITOR "$path_to_dotfiles/user/packages/binaries/$binary_name.nix"
+            print_debug "Edited file"
+            add_to_git "$path_to_dotfiles/user/packages/binaries/$binary_name.nix" "Edited"
+            exit 0
+        else
+            exit 1
+        fi
+    fi
+    
+    case "$state" in
+        Stable)
+            cp $(realpath "$path_to_dotfiles/system/scripts/presets/user/packages/stableBin.nix") $(realpath "$path_to_dotfiles/user/packages/binaries/$binary_name.nix")
+            pattern=".*home.packages = with pkgs-stable; \[.*"
+            ;;
+        Unstable)
+            cp $(realpath "$path_to_dotfiles/system/scripts/presets/user/packages/unstableBin.nix") $(realpath "$path_to_dotfiles/user/packages/binaries/$binary_name.nix")
+            pattern=".*home.packages = with pkgs-unstable; \[.*"
+            ;;
+        Default)
+            cp $(realpath "$path_to_dotfiles/system/scripts/presets/user/packages/defaultBin.nix") $(realpath "$path_to_dotfiles/user/packages/binaries/$binary_name.nix")
+            pattern=".*home.packages = with pkgs; \[.*"
+            ;;
+        *)
+            print_error "That should not have happened. Invalid state!"
+            exit 1
+            ;;
+    esac
+    sed -i "/$pattern/a $insertion" $(realpath "$path_to_dotfiles/user/packages/binaries/$binary_name.nix")
+    edited_file="$path_to_dotfiles/user/packages/binaries/$binary_name.nix"
+    message="Added package \"$name\" as binary to file \"$binary_name\""
+fi
+
+
+if [ -n "$group" ]; then
+    if [ ! -f "$path_to_dotfiles/user/packages/groups/$group.nix" ]; then
+        sh "$path_to_dotfiles/system/scripts/interactive/addPackageGroup.sh" "--name" "$group" "--no-edit" "--no-git" $cmd_debug $cmd_no_usage
+        if [ "$?" -ne 0 ]; then
+            print_error "Something went wrong while creating the group"
+            exit 2
+        fi
+    fi
+    case "$state" in
+        Stable)
+            pattern="with pkgs-stable; \["
+            ;;
+        Unstable)
+            pattern="with pkgs-unstable; \["
+            ;;
+        Default)
+            pattern="home.packages = with pkgs; \["
+            ;;
+        *)
+            print_error "That should not have happened. Invalid state!"
+            exit 1
+            ;;
+    esac
+
+    sed -i "/$pattern/ a\\
+$insertion" "$path_to_dotfiles/user/packages/groups/$group.nix"
+
+    print_debug "Package \"$name\" successfully added to \"$group\""
+    edited_file="$path_to_dotfiles/user/packages/groups/$group.nix"
+    message="Added package \"$name\" to group \"$group\""
+fi
+
+gum confirm "Do you want to edit the file?"
+if [ "$?" -eq 0 ]; then
+    $EDITOR "$edited_file"
+fi
+
+
+add_to_git "$edited_file" "$message"
 
 
 
--- system/scripts/interactive/addPackageGroup.sh
+++ system/scripts/interactive/addPackageGroup.sh
@@ -20,13 +20,13 @@ print_usage_force() {
     echo "  $0 --help, -h      Show this message and exit"
     echo ""
     echo "Options:"
-	echo "  --name,    -n <group name>	  Pre set group name"
-    echo "  --overwrite,   -o               Overwrite group if it already exists"
-    echo "  --no-edit,      -e               Do not ask for edit"
-	echo "  --no-git, 		-g				Do not add to git"
+	echo "  --name,    	   -n <group name>   Pre set group name"
+    echo "  --overwrite,     -o                Overwrite group if it already exists"
+    echo "  --no-edit,       -e                Do not ask for edit"
+	echo "  --no-git, 	   -g                Do not add to git"
     echo ""
-    echo "  --no-usage, -u                  Don't show usage after an error"
-    echo "  --debug,    -d                  Enable debug mode"
+    echo "  --no-usage, -u                     Don't show usage after an error"
+    echo "  --debug,    -d                     Enable debug mode"
     echo ""
 }
 
@@ -75,8 +75,7 @@ while [ $# -gt 0 ]; do
 			;;
 		--name|-n)
 			if [ -n "$2" ]; then
-				hostname="$2"
-                hostname_given=true
+				name="$2"
 				shift
 			else
 				print_error "--name, -n requires a group name"
@@ -153,154 +152,3 @@ if [ "$no_git" = false ]; then
 		git commit -m "[Auto generated] Edited package group \"$name\""
 	fi
 fi
-
-
-: '
-#!/bin/sh
-
-path_to_dotfiles=$(realpath "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../")
-debug=false
-overwrite=false
-no_usage=false
-no_edit=false
-no_add=false
-
-
-cmd_debug=""
-cmd_overwrite=""
-cmd_no_usage=""
-cmd_group=""
-
-
-print_usage_force() {
-    echo "Usage: $0 [options]"
-    echo
-    echo "Options:"
-	echo "  --name, -n <group name>		Pre set group name"
-    echo "  --overwrite, -o        			Overwrite group if it already exists"
-    echo "  --no-edit, -e                   Dont ask to edit after creating new group"
-    echo "  --debug, -d         			Enable debug mode"
-    echo "  --no-usage, -u      			Dont show usage after an error"
-    echo "  --help, -h          			Display this help message and exit"
-    echo
-}
-
-print_usage() {
-    if [ "$no_usage" = false ]; then
-        print_usage_force
-    fi
-} 
-
-print_debug() {
-    if [ "$debug" = true ]; then
-        echo "[Debug]: $1"
-    fi
-}
-
-
-get_group() {
-	cmd_group=""
-	while [ -z "$cmd_group" ]; do
-		read -p "What is the name of the group? " cmd_group
-		if [ -z "$cmd_group" ]; then
-			echo "Please enter a valid group name"
-		fi
-	done
-}
-
-add_to_git() {
-    if [ "$no_add" = false ]; then
-        sh $(realpath "$path_to_dotfiles/system/scripts/helper/getConfirmation.sh") "Do you want to add your changes to git?" --default yes
-        if [ "$?" = 0 ]; then
-            git add $(realpath "$path_to_dotfiles/user/packages/groups/$cmd_group.nix")
-            git commit -m "$1"
-        fi
-    fi  
-}
-
-
-if [ "$1" = "--help" -o "$1" = "-h" ]; then
-    print_usage_force
-    exit 0
-fi
-
-
-
-while [ $# -gt 0 ]; do
-    case "$1" in
-        --debug|-d)
-            debug=true
-			cmd_debug="--debug"
-            ;;
-        --overwrite|-o)
-			overwrite=true
-            cmd_overwrite="--force"
-            ;;
-
-        --no-usage|-u)
-            no_usage=true
-			cmd_no_usage="--no-usage"
-            ;;
-        --no-edit|-e)
-            no_edit=true
-            ;;
-        --no-add)
-            no_add=true
-            ;;
-		--name|-n)
-			if [ -n "$2" ]; then
-				cmd_group="$2"
-				shift
-			else
-				echo "Error: --name or -n requires a name."
-				print_usage
-			fi
-			;;
-        *)
-            echo "Error: Unknown argument '$1'."
-            print_usage
-            exit 1
-            ;;
-    esac
-    shift
-done
-
-if [ -z "$cmd_group" ]; then
-	get_group
-fi
-
-while [ -f $(realpath "$path_to_dotfiles/user/packages/groups/$cmd_group.nix") -a "$overwrite" = false ]; do
-	echo "Warning: A group with the name \"$cmd_group\" already exists"
-	sh $(realpath "$path_to_dotfiles/system/scripts/helper/getOption.sh") "Do you want to overwrite or edit the group?" overwrite/o edit/e --default cancel
-	result="$?"
-	if [ "$result" = 1 ]; then
-        overwrite=true
-        cmd_overwrite="--force"
-    elif [ "$result" = 2 ]; then
-        $EDITOR $(realpath "$path_to_dotfiles/user/packages/groups/$cmd_group.nix")
-        print_debug "File opened for editing"
-        add_to_git "[AM]: Edited \"$cmd_group\"."
-        exit 1
-    else
-        get_group
-    fi
-done
-
-if [ "$overwrite" = true -a -f $(realpath "$path_to_dotfiles/user/packages/groups/$cmd_group.nix") ]; then
-    rm $(realpath "$path_to_dotfiles/user/packages/groups/$cmd_group.nix")
-fi
-
-
-cp $(realpath "$path_to_dotfiles/system/scripts/presets/user/packages/group.nix") $(realpath "$path_to_dotfiles/user/packages/groups/$cmd_group.nix")
-
-
-if [ "$no_edit" = false ]; then
-    sh $(realpath "$path_to_dotfiles/system/scripts/helper/getConfirmation.sh") "Do you want to edit \"$cmd_group\"?"
-    if [ "$?" = 0 ]; then
-        $EDITOR $(realpath "$path_to_dotfiles/user/packages/groups/$cmd_group.nix")
-    fi
-fi
-
-add_to_git "[AM]: Added package group \"$cmd_group\"."
-
-'
