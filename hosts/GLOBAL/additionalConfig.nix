1bd83cf2f3e128d4336d36e106a885d8c67ecce9
Fulcrum7567 <dragon.fighter@outlook.de>
2025-03-05 17:10:45 +0100

    updated locale

--- hosts/GLOBAL/additionalConfig.nix
+++ hosts/GLOBAL/additionalConfig.nix
@@ -1,7 +1,32 @@
 { ... }:
+let
+	english = "en_GB.UTF-8";
+	german = "de_DE.UTF-8";
+in
 {
 	nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
 	nixpkgs.config.allowUnfree = true;
+
+	time.timeZone = "Europe/Berlin";
+	i18n.defaultLocale = english;
+	i18n.extraLocaleSettings = {
+		LC_NUMERIC = english;
+
+		LC_ADDRESS = german;
+		LC_IDENTIFICATION = german;
+		LC_MEASSUREMENT = german;
+		LC_MONETARY = german;
+		LC_NAME = german;
+		LC_PAPER = german;
+		LC_TELEPHONE = german;
+		LC_TIME = german;
+	};
+
+	services.xserver = {
+  	layout = "de";
+    xkbVariant = "";
+  };
+	console.keyMap = "de";
   
 }
